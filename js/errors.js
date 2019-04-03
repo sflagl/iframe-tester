0c5ae2559114d5722cb8bff31c606f6e04bc89cf
sflagl <samanthaflagler@gmail.com>
2019-04-03 15:38:58 -0400

    test approved side

--- js/errors.js
+++ js/errors.js
@@ -1,15 +1,17 @@
 var error = true;
+var approved = false;
 
 
-
-if(error == true){
+if(error == false && approved == true){
 
     $('#img-to-error').hide();
     $( "div" ).removeClass( "beam-add" )
     $('#error-box').show();
     console.log('errors');
 
-}else{
+}else if(error == true && approved == false){
 
     console.log('no errors');
+}else{
+
 }
