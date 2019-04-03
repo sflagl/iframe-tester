ab7a4e80c8841c3b441d170802259c630f3a0832
sflagl <samanthaflagler@gmail.com>
2019-04-03 16:30:12 -0400

    test

--- js/errors.js
+++ js/errors.js
@@ -2,25 +2,52 @@ var error = false;
 var approved = true;
 
 
-if(error == false && approved == true){
+// if(error == false && approved == true){
 
-    $('#img-to-error').hide();
-    $( "div" ).removeClass( "beam-add" )
-    $('#approve-box').show();
-    $('#error-box').hide();
-    console.log('errors');
+//     $('#img-to-error').hide();
+//     $( "div" ).removeClass( "beam-add" )
+//     $('#approve-box').show();
+//     $('#error-box').hide();
+//     console.log('errors');
 
-}else if(error == true && approved == false){
+// }else if(error == true && approved == false){
+
+//     $('#img-to-error').hide();
+//     $( "div" ).removeClass( "beam-add" )
+//     $('#error-box').show();
+//     $('#approve-box').hide();
+//     console.log('no errors');
+// }else{
+
+//     $('#img-to-error').show();
+//     $( "#img-to-error" ).addClass( "beam-add" )
+//     $('#approve-box').hide();
+//     $('#error-box').hide();
+// }
+
+function error(){
+
+    if(error == true){
+
+        $('#img-to-error').hide();
+        $("div").removeClass("beam-add")
+        $('#error-box').show();
+        $('#approve-box').hide();
+
+    } else{
+
+        approve()
+
+    }
+
+}
+
+function approve(){
 
     $('#img-to-error').hide();
     $( "div" ).removeClass( "beam-add" )
     $('#error-box').show();
     $('#approve-box').hide();
-    console.log('no errors');
-}else{
 
-    $('#img-to-error').show();
-    $( "#img-to-error" ).addClass( "beam-add" )
-    $('#approve-box').hide();
-    $('#error-box').hide();
+
 }
