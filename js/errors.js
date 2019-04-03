ea897d403e11a7f2988b9bcec626d5b001ae8b7a
sflagl <samanthaflagler@gmail.com>
2019-04-03 14:37:31 -0400

    test error-box showing

--- js/errors.js
+++ js/errors.js
@@ -1,11 +1,14 @@
 var error = true;
 
 
-if(error == true){
-    $('#img-to-error').hide();
 
+if(error == true){
+
+    $('#img-to-error').hide();
+    $('#error-box').show();
     console.log('errors');
 
 }else{
+
     console.log('no errors');
 }
